561d6e5c5bbe196f611879eabf45ccc3549b4b49
Sun Ziping <sunziping2016@gmail.com>
2020-09-09 23:49:51 +0800

    finish segmentation tool

--- segment-captcha.py
+++ segment-captcha.py
@@ -30,16 +30,20 @@ class MainWindow(QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self)
         self.args = args
         self.image_label = QtWidgets.QLabel()
+        self.find_prev_button = QtWidgets.QPushButton('<<')
         self.prev_button = QtWidgets.QPushButton('<')
         self.reset_button = QtWidgets.QPushButton('Reset')
         self.ok_button = QtWidgets.QPushButton('Ok')
         self.next_button = QtWidgets.QPushButton('>')
+        self.find_next_button = QtWidgets.QPushButton('>>')
 
         self.button_layout = QtWidgets.QHBoxLayout()
+        self.button_layout.addWidget(self.find_prev_button)
         self.button_layout.addWidget(self.prev_button)
         self.button_layout.addWidget(self.reset_button)
         self.button_layout.addWidget(self.ok_button)
         self.button_layout.addWidget(self.next_button)
+        self.button_layout.addWidget(self.find_next_button)
 
         self.layout = QtWidgets.QVBoxLayout()
         self.layout.addWidget(self.image_label, alignment=QtCore.Qt.AlignCenter)
@@ -59,9 +63,29 @@ class MainWindow(QtWidgets.QMainWindow):
         self.statusBar().addPermanentWidget(VLine())
         self.statusBar().addPermanentWidget(self.results_label)
 
+        # connect
+        self.find_prev_button.clicked.connect(self.on_find_prev_click)
+        self.prev_button.clicked.connect(self.on_prev_click)
+        self.reset_button.clicked.connect(self.on_reset_click)
+        self.ok_button.clicked.connect(self.on_ok_click)
+        self.next_button.clicked.connect(self.on_next_click)
+        self.find_next_button.clicked.connect(self.on_find_next_click)
+        self.find_prev_shortcut = QtWidgets.QShortcut(QtGui.QKeySequence.MoveToPreviousWord, self)
+        self.find_prev_shortcut.activated.connect(self.on_find_prev_click)
+        self.prev_shortcut = QtWidgets.QShortcut(QtGui.QKeySequence.MoveToPreviousChar, self)
+        self.prev_shortcut.activated.connect(self.on_prev_click)
+        self.reset_shortcut = QtWidgets.QShortcut(QtGui.QKeySequence.Cancel, self)
+        self.reset_shortcut.activated.connect(self.on_reset_click)
+        self.ok_shortcut = QtWidgets.QShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Return), self)
+        self.ok_shortcut.activated.connect(self.on_ok_click)
+        self.next_shortcut = QtWidgets.QShortcut(QtGui.QKeySequence.MoveToNextChar, self)
+        self.next_shortcut.activated.connect(self.on_next_click)
+        self.find_next_shortcut = QtWidgets.QShortcut(QtGui.QKeySequence.MoveToNextWord, self)
+        self.find_next_shortcut.activated.connect(self.on_find_next_click)
+
         # states
         self.data = {}
-        self.unanswered = []
+        self.questions = []
         self.current = 0
         self.answer = []
         self.mouse_x = None
@@ -69,15 +93,10 @@ class MainWindow(QtWidgets.QMainWindow):
         self.mouse_pressed = False
         self.mouse_moved = False
 
-        # connect
-        self.prev_button.clicked.connect(self.on_prev_click)
-        self.next_button.clicked.connect(self.on_next_click)
-        self.prev_shortcut = QtWidgets.QShortcut(QtGui.QKeySequence.MoveToPreviousChar, self)
-        self.prev_shortcut.activated.connect(self.on_prev_click)
-        self.next_shortcut = QtWidgets.QShortcut(QtGui.QKeySequence.MoveToNextChar, self)
-        self.next_shortcut.activated.connect(self.on_next_click)
-
         self.load_data()
+        if len(self.questions):
+            self.answer = self.data.get(self.questions[self.current], [])[:]
+
         self.update_view()
 
         self.setMouseTracking(True)
@@ -128,18 +147,70 @@ class MainWindow(QtWidgets.QMainWindow):
         self.mouse_moved = True
         self.update_view()
 
+    def on_reset_click(self):
+        if len(self.answer) <= 0:
+            return
+        self.answer = []
+        self.update_view()
+
+    def on_ok_click(self):
+        if not self.is_ok_enabled():
+            return
+        self.data[self.questions[self.current]] = self.answer
+        if self.current >= len(self.questions):
+            self.current = len(self.questions) - 1
+        self.save_data()
+        self.on_find_next_click()
+        self.update_view()
+
+    def on_find_prev_click(self):
+        if len(self.data) >= len(self.questions):
+            msg_box = QtWidgets.QMessageBox()
+            msg_box.setText("Cannot find the next empty captcha")
+            msg_box.exec_()
+            return
+        index = self.current - 1
+        while True:
+            if index < 0:
+                index = len(self.questions) - 1
+            answer = self.data.get(self.questions[index])
+            if answer is None:
+                self.current = index
+                self.answer = []
+                self.update_view()
+                return
+            index -= 1
+
+    def on_find_next_click(self):
+        if len(self.data) >= len(self.questions):
+            msg_box = QtWidgets.QMessageBox()
+            msg_box.setText("Cannot find the next empty captcha")
+            msg_box.exec_()
+            return
+        index = self.current + 1
+        while True:
+            if index >= len(self.questions):
+                index = 0
+            answer = self.data.get(self.questions[index])
+            if answer is None:
+                self.current = index
+                self.answer = []
+                self.update_view()
+                return
+            index += 1
+
     def on_prev_click(self):
         if self.current - 1 < 0:
             return
         self.current -= 1
-        self.answer = self.data.get(self.unanswered[self.current], [])
+        self.answer = self.data.get(self.questions[self.current], [])[:]
         self.update_view()
 
     def on_next_click(self):
-        if self.current + 1 >= len(self.unanswered):
+        if self.current + 1 >= len(self.questions):
             return
         self.current += 1
-        self.answer = self.data.get(self.unanswered[self.current], [])
+        self.answer = self.data.get(self.questions[self.current], [])[:]
         self.update_view()
 
     def load_data(self):
@@ -153,18 +224,17 @@ class MainWindow(QtWidgets.QMainWindow):
                     self.data[key] = value
         except FileNotFoundError:
             pass
-        self.unanswered = []
+        self.questions = []
         for filename in sorted(os.listdir(self.args.captcha_dir)):
             name, ext = os.path.splitext(filename)
             name = name.split('.')
-            if ext == '.jpeg' and len(name) == 2 and len(name[0]) == 32 and \
-                    self.data.get(filename) is None:
-                self.unanswered.append(filename)
+            if ext == '.jpeg' and len(name) == 2 and len(name[0]) == 32:
+                self.questions.append(filename)
 
     def save_data(self):
         with open(os.path.join(self.args.captcha_dir, SEGMENTATION_FILE_NAME), 'w') as f:
             for key in sorted(self.data.keys()):
-                f.write(f'{key}:{",".join([str(i) for i in self.data[key]])}')
+                f.write(f'{key}:{",".join([str(i) for i in self.data[key]])}\n')
 
     def update_view(self):
         self.update_buttons()
@@ -172,20 +242,29 @@ class MainWindow(QtWidgets.QMainWindow):
         self.update_status()
         self.update()
 
+    def is_ok_enabled(self):
+        if self.current < 0 or self.current >= len(self.questions):
+            return False
+        filename = self.questions[self.current]
+        name, ext = os.path.splitext(filename)
+        name = name.split('.')
+        return len(self.answer) == len(name[1])
+
     def update_buttons(self):
-        answer_len = len(self.answer)
+        self.find_prev_button.setEnabled(len(self.data) < len(self.questions))
         self.prev_button.setEnabled(self.current > 0)
-        self.reset_button.setEnabled(answer_len > 0)
-        self.ok_button.setEnabled(4 <= answer_len <= 5)
-        self.next_button.setEnabled(self.current < len(self.unanswered) - 1)
+        self.reset_button.setEnabled(len(self.answer) > 0)
+        self.ok_button.setEnabled(self.is_ok_enabled())
+        self.next_button.setEnabled(self.current < len(self.questions) - 1)
+        self.find_next_button.setEnabled(len(self.data) < len(self.questions))
 
     def update_canvas(self):
-        if self.current < 0 or self.current >= len(self.unanswered):
+        if self.current < 0 or self.current >= len(self.questions):
             self.image_label.setText("All Finished.")
         else:
             pixmap = QtGui.QPixmap(SCALED_WIDTH, SCALED_HEIGHT)
             painter = QtGui.QPainter(pixmap)
-            filename = os.path.join(self.args.captcha_dir, self.unanswered[self.current])
+            filename = os.path.join(self.args.captcha_dir, self.questions[self.current])
             image = QtGui.QPixmap(filename)
             painter.drawPixmap(0, 0, SCALED_WIDTH, SCALED_HEIGHT, image)
             painter.setPen(QtGui.QPen(QtGui.QColor('green'), LINE_WIDTH))
@@ -198,15 +277,15 @@ class MainWindow(QtWidgets.QMainWindow):
             self.image_label.setPixmap(pixmap)
 
     def update_status(self):
-        self.finished_label.setText(f'Prog: {len(self.data)}/{len(self.data) + len(self.unanswered)}')
-        self.index_label.setText(f'Idx: {self.current + 1}/{len(self.unanswered)}')
+        self.finished_label.setText(f'Prog: {len(self.data)}/{len(self.questions)}')
+        self.index_label.setText(f'Idx: {self.current + 1}/{len(self.questions)}')
         self.pos_label.setText(f'X: {"N/A" if self.mouse_x is None else self.mouse_x}')
         self.results_label.setText(f'Pos: [{",".join([str(i) for i in self.answer])}]')
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--captcha_dir', default='captcha', help='path to the captcha')
+    parser.add_argument('--captcha_dir', default='captcha-test', help='path to the captcha')
     args, unparsed_args = parser.parse_known_args()
     app = QtWidgets.QApplication(sys.argv[:1] + unparsed_args)
     main_window = MainWindow(args)
